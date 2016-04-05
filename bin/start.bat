340874548ae786337db58d51145f87f737d56c10
baayso <baayso@qq.com>
2016-04-05 08:58:25 +0800

    修改启动脚本，增加‘端口’参数。

--- bin/start.bat
+++ bin/start.bat
@@ -1,14 +1,18 @@
 @echo off
+
 echo [Pre-Requirement] Makesure install JDK 8.0+ and set the JAVA_HOME.
 
-set PROFILE=%1%
+set PORT=%1%
+set PROFILE=%2%
 
-if "%1%" == "" (set PROFILE=dev)
+if "%1%" == "" (set PORT=6666)
+
+if "%2%" == "" (set PROFILE=pro)
 
 set JAVA_OPTS=-server -Xms2048m -Xmx2048m -Djava.awt.headless=true -Djava.net.preferIPv4Stack=true
 echo %JAVA_OPTS%
 
-set APP_OPTS=--spring.profiles.active=%PROFILE%
+set APP_OPTS=--server.port=%PORT% --spring.profiles.active=%PROFILE%
 echo %APP_OPTS%
 
 echo java %JAVA_OPTS% -jar spring-boot-demo.jar %APP_OPTS%
--- bin/start.sh
+++ bin/start.sh
@@ -2,16 +2,21 @@
 
 echo "[Pre-Requirement] Makesure install JDK 8.0+ and set the JAVA_HOME."
 
-PROFILE=$1
+PORT=$1
+PROFILE=$2
+
+if [ -z ${PORT} ]; then
+    PROFILE="6666"
+fi
 
 if [ -z ${PROFILE} ]; then
-    PROFILE="dev"
+    PROFILE="pro"
 fi
 
 JAVA_OPTS="-server -Xms2048m -Xmx2048m -Djava.awt.headless=true -Djava.net.preferIPv4Stack=true"
 echo ${JAVA_OPTS}
 
-APP_OPTS="--spring.profiles.active=${PROFILE}"
+APP_OPTS="--server.port=${PORT} --spring.profiles.active=${PROFILE}"
 echo ${APP_OPTS}
 
 echo "java ${JAVA_OPTS} -jar spring-boot-demo.jar ${APP_OPTS}"
