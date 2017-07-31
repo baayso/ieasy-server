50a15fb50741efc5238e6e871b0bc636f23db0aa
baayso <baayso@outlook.com>
2017-07-31 14:19:44 +0800

    Add stop.sh

---
+++ bin/stop.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PID=$(ps -ef | grep spring-boot-demo.jar | grep -v grep | awk '{ print $2 }')
+
+if [ -z "$PID" ]; then
+    echo Application is already stopped.
+else
+    echo kill $PID
+    kill $PID
+fi
