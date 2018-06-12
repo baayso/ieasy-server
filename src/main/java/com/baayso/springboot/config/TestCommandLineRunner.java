b71ec921775b381e6fa489935c6dda1678c1589e
baayso <baayso@outlook.com>
2018-06-12 09:37:11 +0800

    优化代码。

--- src/main/java/com/baayso/springboot/config/TestCommandLineRunner.java
+++ src/main/java/com/baayso/springboot/config/TestCommandLineRunner.java
@@ -4,19 +4,22 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * 服务启动时执行。
  *
  * @author ChenFangjie (2018/3/23 23:05)
  * @since 2.0.0
  */
+@Slf4j
 @Component
 @Order(value = 1)
 public class TestCommandLineRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println(">>>>>>>>>>>>>>> 服务启动时执行，执行加载数据等操作 <<<<<<<<<<<<<");
+        log.info(">>>>>>>>>>>>>>> 服务启动时执行，执行加载数据等操作 <<<<<<<<<<<<<");
     }
 
 }
